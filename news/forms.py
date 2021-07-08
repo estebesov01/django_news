712a4c42a4aac1f67578988b72913d59491bd1ff
estebesov01 <estebesov7777@gmail.com>
2021-07-08 19:14:26 +0600

    my project

---
+++ news/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms import widgets
+from .models import News, Category
+from django.core.exceptions import ValidationError
+import re
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widget = {
+           'title': forms.TextInput(attrs={'class': 'form-control'}),
+           'content': forms.Textarea(attrs={'class': 'form-control', 'row': 5}),
+           'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Давай без цифр')
+        return title
+
